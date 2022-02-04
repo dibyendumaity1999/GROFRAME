5d6337ec071cb4d2ff6ae2e5c43e98c25c58d362
Dibyendu Maity <60811558+dibyendumaity1999@users.noreply.github.com>
2022-02-05 00:05:22 +0530

    Update __init__.py

--- GROFRAME/__init__.py
+++ GROFRAME/__init__.py
@@ -1,4 +1,6 @@
 # This is so that you can import ppack or import average from ppack
 # in stead of from ppack.functions import average
 
+import numpy as np
+import pandas as pd
 from .GROFRAME import GROFRAME
